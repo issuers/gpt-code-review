2b72c729b31b28d43f848675f2c6a736819db4e3
anshuman-airwallex <anshuman@airwallex.com>
2023-06-22 16:46:42 +1000

    test workflow

--- app/src/main/kotlin/com/airwallex/issuing/gpt/App.kt
+++ app/src/main/kotlin/com/airwallex/issuing/gpt/App.kt
@@ -12,16 +12,4 @@ class App {
 
 fun main() {
     println(App().greeting)
-
-    if (2 == 3) {
-        if (4 == 5) {
-            val i = 10
-            println(i)
-        }
-    }
-    while (true) {
-        while (true) {
-            println("test")
-        }
-    }
 }
