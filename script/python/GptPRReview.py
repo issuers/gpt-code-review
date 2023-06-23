3c441a83361bd421e81e1dea8f0844ad1c720d47
Kaifu Ji <kaifu.ji@airwallex.com>
2023-06-23 11:27:17 +1000

    update script

--- script/python/GptPRReview.py
+++ script/python/GptPRReview.py
@@ -1,24 +1,19 @@
-import sys
-import requests
 import json
+import requests
+import sys
 
-PR_URL=sys.argv[1].rstrip()
+PR_URL = sys.argv[1].rstrip()
 
 patch_file = requests.get(PR_URL + ".patch")
 api_token = sys.argv[2]
 headers = {"Content-Type": "application/json", "Authorization": "Bearer " + api_token}
-sys_msg = 'You are a programming code change reviewer, provide feedback on the code changes given. Do not ' \
+sys_msg = 'You are a programming code change review helper, provide summary for the change to help people understand ' \
+          'the change. Do not ' \
           'introduce yourself.'
 prompt = 'your task is:' \
-         '- Review the code changes and provide feedback.' \
-         '- If there are any bugs, highlight them.' \
-         '- Provide details on missed use of best-practices.' \
-         '- Does the code do what it says in the commit messages?' \
-         '- Do not highlight minor issues and nitpicks.' \
-         '- Use bullet points if you have multiple comments.' \
-         '- Provide security recommendations if there are any.' \
-         '-use markup as your response format and highlight any code part' \
-         'You are provided with the code changes (diffs) in a unidiff format.' \
+         '- Write some summary for the complex functions' \
+         '- Use markdown as your response format and highlight any code part' \
+         ' You are provided with the code changes (diffs) in a unidiff format.' \
          + patch_file.text
 data = {
     'model': 'gpt-3.5-turbo-16k-0613',
