c3c3d39f5694323574980c9fe1ac42223b9bb8ac
Kaifu Ji <kaifu.ji@airwallex.com>
2023-06-23 10:32:15 +1000

    feat: [] some functions

--- app/src/main/kotlin/com/airwallex/issuing/gpt/App.kt
+++ app/src/main/kotlin/com/airwallex/issuing/gpt/App.kt
@@ -11,30 +11,6 @@ class App {
 }
 
 fun main() {
-    println(App().greeting)
-
-    if (2 == 3) {
-        if (4 == 5) {
-            val i = 10
-            println(i)
-        }
-    }
-    while (true) {
-        while (true) {
-            println("test")
-        }
-    }
-
-
-
-    val r = 40/0
-    println(r)
-
-    val GITHUB_API_KEY = "adkjhfdiuhAUUDUIDH298y32387232"
-    println(GITHUB_API_KEY)
-
-    val CREDIT_CARD = "4444333322221111"
-    println(CREDIT_CARD)
 }
 
 fun notUsedMethod() {
